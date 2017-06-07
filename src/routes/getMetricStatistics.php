6b95f9dcad503c08bf3eb0b8ad44188fb3012da7
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-06-07 12:49:15 +0300

    refactor update

--- src/routes/getMetricStatistics.php
+++ src/routes/getMetricStatistics.php
@@ -1,54 +1,55 @@
 <?php
 
 $app->post('/api/AmazonCloudWatch/getMetricStatistics', function ($request, $response, $args) {
-    
+
     $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, ['apiKey','apiSecret','region','endTime','metricName','namespace','period','startTime']);
-    if(!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback']=='error') {
+    $validateRes = $checkRequest->validate($request, ['apiKey', 'apiSecret', 'region', 'endTime', 'metricName', 'namespace', 'period', 'startTime']);
+    if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
     } else {
         $post_data = $validateRes;
     }
-    
+
     $credentials = new Aws\Credentials\Credentials($post_data['args']['apiKey'], $post_data['args']['apiSecret']);
 
     $client = new Aws\CloudWatch\CloudWatchClient([
-        'version'     => 'latest',
-        'region'      => $post_data['args']['region'],
+        'version' => 'latest',
+        'region' => $post_data['args']['region'],
         'credentials' => $credentials
     ]);
-    
-    $body['EndTime'] = $post_data['args']['endTime'];
+    $endDateTime = new DateTime($post_data['args']['endTime']);
+    $body['EndTime'] = $endDateTime->format('Y-m-d\TH:i:s\Z');
     $body['MetricName'] = $post_data['args']['metricName'];
     $body['Namespace'] = $post_data['args']['namespace'];
     $body['Period'] = $post_data['args']['period'];
-    $body['StartTime'] = $post_data['args']['startTime'];
-    
-    if(!empty($post_data['args']['dimensions'])) {
+    $startDateTime = new DateTime($post_data['args']['startTime']);
+    $body['StartTime'] = $startDateTime->format('Y-m-d\TH:i:s\Z');
+
+    if (!empty($post_data['args']['dimensions'])) {
         $body['Dimensions'] = $post_data['args']['dimensions'];
     }
-    if(!empty($post_data['args']['extendedStatistics'])) {
+    if (!empty($post_data['args']['extendedStatistics'])) {
         $body['ExtendedStatistics'] = $post_data['args']['extendedStatistics'];
     }
-    if(!empty($post_data['args']['statistics'])) {
+    if (!empty($post_data['args']['statistics'])) {
         $body['Statistics'] = $post_data['args']['statistics'];
     }
-    if(!empty($post_data['args']['unit'])) {
+    if (!empty($post_data['args']['unit'])) {
         $body['Unit'] = $post_data['args']['unit'];
     }
-    
+
     try {
-        if(!empty($body)) {
+        if (!empty($body)) {
             $res = $client->getMetricStatistics(
                 $body
             )->toArray();
         } else {
             $res = $client->getMetricStatistics()->toArray();
         }
-        
+
         $result['callback'] = 'success';
         $result['contextWrites']['to'] = is_array($res) ? $res : json_decode($res);
-        if(empty($result['contextWrites']['to'])) {
+        if (empty($result['contextWrites']['to'])) {
             $result['contextWrites']['to']['status_msg'] = "Api return no results";
         }
     } catch (S3Exception $e) {
@@ -65,7 +66,7 @@ $app->post('/api/AmazonCloudWatch/getMetricStatistics', function ($request, $res
         $result['contextWrites']['to']['status_code'] = 'API_ERROR';
         $result['contextWrites']['to']['status_msg'] = $e->getMessage();
     }
-    
+
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
-    
+
 });
--- src/routes/putMetricData.php
+++ src/routes/putMetricData.php
@@ -1,56 +1,62 @@
 <?php
 
 $app->post('/api/AmazonCloudWatch/putMetricData', function ($request, $response, $args) {
-    
+
     $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, ['apiKey','apiSecret','region','metricName','namespace']);
-    if(!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback']=='error') {
+    $validateRes = $checkRequest->validate($request, ['apiKey', 'apiSecret', 'region', 'metricName', 'namespace']);
+    if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
     } else {
         $post_data = $validateRes;
     }
-    
+
     $credentials = new Aws\Credentials\Credentials($post_data['args']['apiKey'], $post_data['args']['apiSecret']);
 
     $client = new Aws\CloudWatch\CloudWatchClient([
-        'version'     => 'latest',
-        'region'      => $post_data['args']['region'],
+        'version' => 'latest',
+        'region' => $post_data['args']['region'],
         'credentials' => $credentials
     ]);
-    
+
     $body['Namespace'] = $post_data['args']['namespace'];
     $metricData['MetricName'] = $post_data['args']['metricName'];
-    
-    if(!empty($post_data['args']['dimensions'])) {
+
+    if (!empty($post_data['args']['dimensions'])) {
         $metricData['Dimensions'] = $post_data['args']['dimensions'];
     }
-    if(!empty($post_data['args']['statisticValues'])) {
+    if (!empty($post_data['args']['statisticValues'])) {
         $metricData['StatisticValues'] = $post_data['args']['statisticValues'];
     }
-    if(!empty($post_data['args']['timestamp'])) {
-        $metricData['Timestamp'] = $post_data['args']['timestamp'];
+    if (!empty($post_data['args']['timestamp'])) {
+        if (is_numeric($post_data['args']['timestamp'])) {
+            $timestamp = $post_data['args']['timestamp'];
+        } else {
+            $dateTime = new DateTime($post_data['args']['timestamp']);
+            $timestamp = $dateTime->format('U');
+        }
+        $metricData['Timestamp'] = $timestamp;
     }
-    if(!empty($post_data['args']['unit'])) {
+    if (!empty($post_data['args']['unit'])) {
         $metricData['Unit'] = $post_data['args']['unit'];
     }
-    if(!empty($post_data['args']['value'])) {
+    if (!empty($post_data['args']['value'])) {
         $metricData['Value'] = $post_data['args']['value'];
     }
-    
+
     $body['MetricData'][] = $metricData;
-    
+
     try {
-        if(!empty($body)) {
+        if (!empty($body)) {
             $res = $client->putMetricData(
                 $body
             )->toArray();
         } else {
             $res = $client->putMetricData()->toArray();
         }
-        
+
         $result['callback'] = 'success';
         $result['contextWrites']['to'] = is_array($res) ? $res : json_decode($res);
-        if(empty($result['contextWrites']['to'])) {
+        if (empty($result['contextWrites']['to'])) {
             $result['contextWrites']['to']['status_msg'] = "Api return no results";
         }
     } catch (S3Exception $e) {
@@ -67,7 +73,7 @@ $app->post('/api/AmazonCloudWatch/putMetricData', function ($request, $response,
         $result['contextWrites']['to']['status_code'] = 'API_ERROR';
         $result['contextWrites']['to']['status_msg'] = $e->getMessage();
     }
-    
+
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
-    
+
 });
